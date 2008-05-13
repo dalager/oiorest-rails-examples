82b2400aa52da8794289b1eafe59a79ecb01bf86
Olle Jonsson <olle.jonsson@gmail.com>
2008-05-14 01:25:38 +0200

    default route added (kommuner)

--- config/routes.rb
+++ config/routes.rb
@@ -24,8 +24,8 @@ ActionController::Routing::Routes.draw do |map|
   #     admin.resources :products
   #   end
 
-  # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
-  # map.root :controller => "welcome"
+  # Root of site, explanatory
+  map.root :controller => "kommuner"
 
   # See how all your routes lay out with "rake routes"
 
