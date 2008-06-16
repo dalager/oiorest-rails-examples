d7e8583d6dfbabff5389cdd7a5c64f1c285bbbe1
Christian Dalager <dalager@dalager.local>
2008-06-16 22:45:29 +0200

    Added better querystring parsing. Now it's possible to parse a two-word streetname like "tuborg havnevej 19" Added ajax progress indication

--- app/controllers/adresser_controller.rb
+++ app/controllers/adresser_controller.rb
@@ -6,28 +6,24 @@ class AdresserController < ApplicationController
   def query
     q = params[:query]
     
-    # "lyongade 19, 2300"
-    
+    # expecting address in format "Lyongade 19, 2300" or less
     vej,postnr = q.split(',')
-    vejnavn, husnr = vej.split(' ')
+    vejnavn, husnr = vej.split(/(\d{1,4})/)
     
     parameters = {}
     if(vejnavn)
-      parameters[:vejnavn] = vejnavn
+      parameters[:vejnavn] = vejnavn.strip
     end
     if(husnr)
-      parameters[:husnr] = husnr
+      parameters[:husnr] = husnr.strip
     end
     if(postnr)
-      parameters[:postnr] = postnr
+      parameters[:postnr] = postnr.strip
     end
     
     adresses = Adresse.find(:all,:params => parameters)
     
-    
-    
-    
-    render :partial => "adresse", :collection  => adresses
+    render :partial => "adresser", :locals  => {:adresser  => adresses}
   end
 
 end
